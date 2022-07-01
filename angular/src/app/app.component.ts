ba99c2618031b45926a0848c9f741a24a4e24b5e
Victor <victor.hernandez@optimizely.com>
2022-07-01 13:31:32 -0400

    add angular

---
+++ angular/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import  "../../../../header/webcomponent/opti-header/dist/src";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+
+export class AppComponent {
+  title = 'angular';
+}
